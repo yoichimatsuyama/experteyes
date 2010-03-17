50aad8d621e40c8ee2098fcccc3746b6c30f3b1d
akavipat <akavipat@6215b030-ad11-11dd-8324-037e16aeffe8>
2010-03-17 19:00:08 +0000

    Rearragne things so that the old data won't break

---
+++ trunk/src/eyetrackercalibrator/gui/util/FrameMarker.java
@@ -0,0 +1,98 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eyetrackercalibrator.gui.util;
+
+import org.jfree.chart.plot.IntervalMarker;
+
+/**
+ * Base class for all classes that used to mark frames
+ * @author ruj
+ */
+public class FrameMarker {
+
+    public int startEyeFrame = 0;
+    public int stopEyeFrame = 0;
+    public int startSceneFrame = 0;
+    public int stopSceneFrame = 0;
+    protected IntervalMarker intervalMarker = null;
+    /** Used for adjusting start and stop */
+    protected int referenceFrame = 0;
+    protected int referenceEyeFrame = 0;
+    protected int referenceSceneFrame = 0;
+
+    @Override
+    public boolean equals(Object obj) {
+        FrameMarker o = (FrameMarker) obj;
+        return (o.startEyeFrame == startEyeFrame &&
+                o.stopEyeFrame == stopEyeFrame &&
+                o.startSceneFrame == startSceneFrame &&
+                o.stopSceneFrame == stopSceneFrame);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + this.startEyeFrame;
+        hash = 53 * hash + this.stopEyeFrame;
+        hash = 53 * hash + this.startSceneFrame;
+        hash = 53 * hash + this.stopSceneFrame;
+        return hash;
+    }
+
+    /**
+     * Method for marking end frame and set the interval marker accordingly.
+     * The method takes care of making sure that starting frame is smaller
+     * than the end frame.  The method only works if you use it after
+     * calling setStartFrame.
+     */
+    public void setEndFrame(int currentFrame, int eyeFrame, int sceneFrame) {
+        int endRef = 0;
+        int startRef = 0;
+
+        if (currentFrame >= this.referenceFrame) {
+            /** Normal start and end case */
+            startRef = this.referenceFrame;
+            endRef = currentFrame;
+            this.startEyeFrame = this.referenceEyeFrame;
+            this.stopEyeFrame = eyeFrame;
+            this.startSceneFrame = this.referenceSceneFrame;
+            this.stopSceneFrame = sceneFrame;
+        } else {
+            startRef = currentFrame;
+            endRef = this.referenceFrame;
+            this.startEyeFrame = eyeFrame;
+            this.stopEyeFrame = this.referenceEyeFrame;
+            this.startSceneFrame = sceneFrame;
+            this.stopSceneFrame = this.referenceSceneFrame;
+        }
+
+        if (this.intervalMarker != null) {
+            this.intervalMarker.setEndValue(endRef);
+            this.intervalMarker.setStartValue(startRef);
+        }
+    }
+
+    public void setStartFrame(int currentFrame, int eyeFrame, int sceneFrame) {
+        this.startEyeFrame = eyeFrame;
+        this.startSceneFrame = sceneFrame;
+        if (this.intervalMarker != null) {
+            this.intervalMarker.setStartValue(currentFrame);
+        }
+
+        this.referenceFrame = currentFrame;
+        this.referenceEyeFrame = eyeFrame;
+        this.referenceSceneFrame = sceneFrame;
+    }
+
+    public IntervalMarker getIntervalMarker() {
+        return intervalMarker;
+    }
+
+    public void setIntervalMarker(IntervalMarker intervalMarker) {
+        this.intervalMarker = intervalMarker;
+        intervalMarker.setAlpha(0.2f);
+    }
+
+}
---
+++ trunk/src/eyetrackercalibrator/trialmanaging/TrialMarker.java
@@ -0,0 +1,145 @@
+/*
+* Copyright (c) 2009 by Thomas Busey and Ruj Akavipat
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the Experteyes nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY Thomas Busey and Ruj Akavipat ''AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL Thomas Busey and Ruj Akavipat BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eyetrackercalibrator.trialmanaging;
+
+import eyetrackercalibrator.gui.util.FrameMarker;
+import org.jdom.Element;
+
+/**
+ *
+ * @author eeglab
+ */
+public class TrialMarker extends FrameMarker implements Comparable {
+
+    /** Trial information */
+    public String label;
+    public boolean isBadTrial = false;
+
+    /** For storing and loading information to the XML format */
+    public final static String ELEMENT_NAME = "Trial";
+    public final static String EYE_FRAME_ELEMENT_NAME = "EyeFrames";
+    public final static String SCENE_FRAME_ELEMENT_NAME = "ScreenFrames";
+    public final static String FROM_ATTRIBUTE = "from";
+    public final static String TO_ATTRIBUTE = "to";
+    public final static String NAME_ATTRIBUTE = "name";
+    public final static String BAD_TRIAL_ATTRIBUTE = "is_bad";
+
+
+    public TrialMarker() {
+    }
+
+    /**
+     * Create trial marking from element
+     */
+    public TrialMarker(Element e) {
+        String string = null;
+
+        // Set eye frame
+        Element element = e.getChild(EYE_FRAME_ELEMENT_NAME);
+        if(element != null){
+            string = element.getAttributeValue(FROM_ATTRIBUTE);
+            if(string != null){
+                startEyeFrame = Integer.parseInt(string);
+            }
+            string = element.getAttributeValue(TO_ATTRIBUTE);
+            if(string != null){
+                stopEyeFrame = Integer.parseInt(string);
+            }
+        }
+
+        // Set screen frame
+        element = e.getChild(SCENE_FRAME_ELEMENT_NAME);
+        if(element != null){
+            string = element.getAttributeValue(FROM_ATTRIBUTE);
+            if(string != null){
+                startSceneFrame = Integer.parseInt(string);
+            }
+            string = element.getAttributeValue(TO_ATTRIBUTE);
+            if(string != null){
+                stopSceneFrame = Integer.parseInt(string);
+            }
+        }
+       
+        // Set up flags
+        label = e.getAttributeValue(NAME_ATTRIBUTE);
+        string = e.getAttributeValue(BAD_TRIAL_ATTRIBUTE);
+        if(string != null){
+            isBadTrial = Boolean.parseBoolean(string);
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        String retValue = "<html>";
+
+        if (label != null) {
+            retValue = retValue.concat(label + ":<br>");
+        }
+
+        retValue = retValue.concat(
+                "From:" + startEyeFrame + ":" + startSceneFrame + "(eye:scene)<br>");
+        retValue = retValue.concat(
+                "To:" + stopEyeFrame + ":" + stopSceneFrame + "(eye:scene)<br>");
+        retValue = retValue.concat("</html>");
+
+        return retValue;
+    }
+
+    public Element toElement() {
+        Element root = new Element(ELEMENT_NAME);
+        // Add eye frame range
+        Element elm = new Element(EYE_FRAME_ELEMENT_NAME);
+        elm.setAttribute(FROM_ATTRIBUTE, String.valueOf(startEyeFrame));
+        elm.setAttribute(TO_ATTRIBUTE, String.valueOf(stopEyeFrame));
+        root.addContent(elm);
+
+        // Add screen frame range
+        elm = new Element(SCENE_FRAME_ELEMENT_NAME);
+        elm.setAttribute(FROM_ATTRIBUTE, String.valueOf(startSceneFrame));
+        elm.setAttribute(TO_ATTRIBUTE, String.valueOf(stopSceneFrame));
+        root.addContent(elm);
+
+        root.setAttribute(NAME_ATTRIBUTE, label);
+        root.setAttribute(BAD_TRIAL_ATTRIBUTE, String.valueOf(isBadTrial));
+
+        return root;
+    }
+
+    public int compareTo(Object o) {
+        TrialMarker m = (TrialMarker) o;
+        if(this.startEyeFrame == m.startEyeFrame){
+            return this.label.compareTo(m.label);
+        }else{
+            return this.startSceneFrame - m.startSceneFrame;
+        }
+    }
+}
